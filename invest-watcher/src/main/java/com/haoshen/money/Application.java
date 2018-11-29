1fab6c1c12b047b63d6ad3134ce13c965efa4592
wanghao71 <wanghao71@baidu.com>
2018-11-29 10:27:08 +0800

    fix

--- invest-watcher/src/main/java/com/haoshen/money/Application.java
+++ invest-watcher/src/main/java/com/haoshen/money/Application.java
@@ -3,11 +3,19 @@ package com.haoshen.money;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.haoshen.money.mapper")
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override       //为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 }
